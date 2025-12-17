be1ce50aa4adbf46b0d6acc2ca34a44073164dc1
m-max67 <mnanrhay@gmail.com>
2025-12-16 18:17:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function showPage(pageId) {
   document.querySelectorAll('.page').forEach(page => {
     page.classList.remove('active');
   });
+  document.getElementById(pageId).classList.add('active');
+}
 
-  const page = document.getElementById(pageId);
-  if (page) {
-    page.classList.add('active');
-  }
+function play(name) {
+  alert(name + " game coming next!");
 }
